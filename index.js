5d2a27b135311246f229a772a971b95ef63d8d61
NBaLogn <36346192+NBaLogn@users.noreply.github.com>
2023-06-04 21:49:29 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const {readFile} = require('fs').promises;
+
+app.get('/', async (request, response) => {
+    response.send(await readFile('./index.html', 'utf8'));
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('app available on http://localhost:3000'));
